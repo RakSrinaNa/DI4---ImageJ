c32a78c5535fd0a9f173df8f4fc53c54c19f04e3
Thomas COUCHOUD <zerderr@gmail.com>
2018-04-10 08:18:29 +0200

    Base

---
+++ src/main/java/fr/mrcraftcod/imagej/Main.java
@@ -0,0 +1,10 @@
+package fr.mrcraftcod.imagej;
+
+/**
+ * Created by Thomas Couchoud (MrCraftCod - zerderr@gmail.com) on 10/04/2018.
+ *
+ * @author Thomas Couchoud
+ * @since 2018-04-10
+ */
+public class Main
+{}
