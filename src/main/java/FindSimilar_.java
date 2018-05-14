0f39b2a46730744c885bfff0202d08a1932fcef2
Thomas COUCHOUD <zerderr@gmail.com>
2018-05-14 11:51:35 +0200

    ROLLBACK

--- src/main/java/FindSimilar_.java
+++ src/main/java/FindSimilar_.java
@@ -1,47 +1,38 @@
 import ij.IJ;
 import ij.ImagePlus;
 import ij.plugin.filter.PlugInFilter;
+import ij.process.ImageConverter;
 import ij.process.ImageProcessor;
 import java.io.File;
-import java.util.List;
+import java.util.*;
 
 public class FindSimilar_ implements PlugInFilter
 {
 	public void run(ImageProcessor ip)
 	{
-		IJ.showMessage("HELLO");
-		// JFileChooser chooser = new JFileChooser();
-		// chooser.setCurrentDirectory(new java.io.File("."));
-		// chooser.setDialogTitle("Selectionnez un répertoire avec des images");
-		// chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-		// chooser.setAcceptAllFileFilterUsed(false);
-		// chooser.showDialog(null, "OK");
-		// File path = chooser.getSelectedFile();
-		// if(path != null)
-		// {
-		// 	File[] files = path.listFiles();
-		// 	if(files != null && files.length != 0)
-		// 	{
-		// 		double avgReal = AverageNdg(ip);
-		// 		Map<Double, List<File>> similarities = new HashMap<Double, List<File>>();
-		// 		//initialization variables locales
-		// 		for(File file : files)
-		// 		{
-		// 			// creation d’une image temporaire
-		// 			ImagePlus tempImg = new ImagePlus(file.getAbsolutePath());
-		// 			new ImageConverter(tempImg).convertToGray8();
-		// 			ImageProcessor ipTemp = tempImg.getProcessor();
-		// 			double dst = Math.abs(AverageNdg(ipTemp) - avgReal);
-		// 			if(!similarities.containsKey(dst))
-		// 				similarities.put(dst, new ArrayList<File>());
-		// 			similarities.get(dst).add(file);
-		// 		}
-		// 		double minDist = Collections.min(similarities.keySet());
-		// 		IJ.showMessage("L’image la plus proche est " + getBeautifulFiles(similarities.get(minDist)) + " avec une distance de" + minDist);
-		// 	}
-		// }
-		// else
-		// 	IJ.showMessage("Merci de sélectionner un dossier avec des images à comparer");
+		String path = IJ.getDirectory("current");
+		File[] files = new File(path == null ? "." : path).listFiles();
+		if(files != null && files.length != 0)
+		{
+			double avgReal = AverageNdg(ip);
+			Map<Double, List<File>> similarities = new HashMap<Double, List<File>>();
+			//initialization variables locales
+			for(File file : files)
+			{
+				// creation d’une image temporaire
+				ImagePlus tempImg = new ImagePlus(file.getAbsolutePath());
+				new ImageConverter(tempImg).convertToGray8();
+				ImageProcessor ipTemp = tempImg.getProcessor();
+				double dst = Math.abs(AverageNdg(ipTemp) - avgReal);
+				if(!similarities.containsKey(dst))
+					similarities.put(dst, new ArrayList<File>());
+				similarities.get(dst).add(file);
+			}
+			double minDist = Collections.min(similarities.keySet());
+			IJ.showMessage("L’image la plus proche est " + getBeautifulFiles(similarities.get(minDist)) + " avec une distance de" + minDist);
+		}
+		else
+			IJ.showMessage("Merci de sélectionner un dossier avec des images à comparer");
 	}
 	
 	private String getBeautifulFiles(List<File> files)
