278b29c42cfe667e33899ae58648ce7af7328d9c
Thomas COUCHOUD <zerderr@gmail.com>
2018-05-25 08:24:43 +0200

    BOOM

--- src/main/java/Plugin_.java
+++ src/main/java/Plugin_.java
@@ -137,11 +137,11 @@ public class Plugin_ implements PlugInFilter
 		StringBuilder builder = new StringBuilder();
 		for(Color color : colors.keySet())
 			if(colors.get(color) > threshold * count)
-				builder.append(baseColors.get(color)).append(":").append(colors.get(color) / (double) count).append(", ");
-
+				builder.append(baseColors.get(color)).append(":").append(String.format("%.2f", colors.get(color) / (double) count)).append(", ");
+		
 		if(builder.length() > 1)
 			builder.delete(builder.length() - 2, builder.length());
-
+		
 		return builder.toString();
 	}
 	
@@ -162,8 +162,67 @@ public class Plugin_ implements PlugInFilter
 				ip2.putPixel(i, j, c.getRGB());
 			}
 		}
-		ip.insert(ip2, 0, 0);
+		AWTImageComponent component = new AWTImageComponent(ip2.createImage());
+		ScrollPane scrollPane = new ScrollPane();
+		scrollPane.add(component);
+		Frame f = new Frame();
+		f.addWindowListener(new WindowAdapter()
+		{
+			public void windowClosing(WindowEvent e)
+			{
+				System.exit(0);
+			}
+		});
+		f.add(scrollPane);
+		f.setSize(400,400);
+		f.setLocation(200,200);
+		f.setVisible(true);
 		return colors;
+	}
+	
+	class AWTImageComponent extends Panel
+	{
+		Image image;
+		
+		public AWTImageComponent(Image image)
+		{
+			loadImage(image);
+		}
+		
+		public void paint(Graphics g)
+		{
+			super.paint(g);
+			int w = getWidth();
+			int h = getHeight();
+			int imageWidth = image.getWidth(this);
+			int imageHeight = image.getHeight(this);
+			int x = (w - imageWidth)/2;
+			int y = (h - imageHeight)/2;
+			g.drawImage(image, x, y, this);
+		}
+		
+		/**
+		 * For the ScrollPane or other Container.
+		 */
+		public Dimension getPreferredSize()
+		{
+			return new Dimension(image.getWidth(this), image.getHeight(this));
+		}
+		
+		private void loadImage(Image image)
+		{
+			this.image = image;
+			MediaTracker tracker = new MediaTracker(this);
+			tracker.addImage(image, 0);
+			try
+			{
+				tracker.waitForID(0);
+			}
+			catch(InterruptedException ie)
+			{
+				System.out.println("interrupt: " + ie.getMessage());
+			}
+		}
 	}
 	
 	private Color getClosestColor(int x, int y, int i)
